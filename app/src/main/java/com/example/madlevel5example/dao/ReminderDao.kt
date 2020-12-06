9b4dd5651c5b69d19046834389ae58f3667b41f6
Aron Schiphof <aron.schiphof@gmail.com>
2020-12-06 18:33:31 +0100

    Refactored the reminders app to use the Android Architecture Components

---
+++ app/src/main/java/com/example/madlevel5example/dao/ReminderDao.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel5example.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.madlevel5example.model.Reminder
+
+@Dao
+interface ReminderDao {
+
+    @Query("Select * FROM reminderTable")
+    fun getAllReminders(): LiveData<List<Reminder>>
+
+    @Insert
+    suspend fun insertReminder(reminder: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminder: Reminder)
+
+    @Update
+    suspend fun updateReminder(reminder: Reminder)
+}
